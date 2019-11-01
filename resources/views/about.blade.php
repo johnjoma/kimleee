a327abc46012e6e7ae3983d516fc0712ebd76001
johnjoma <irungujohnkimani@gmail.com>
2019-11-01 20:02:49 +0300

    kimyech

---
+++ resources/views/about.blade.php
@@ -0,0 +1,271 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Real Estate Template | About</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/theme-1.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+        <!--[if gte IE 9]>
+        	<link rel="stylesheet" type="text/css" href="css/ie9.css" />
+		<![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+    	<div class="loader">
+    		<div class="spinner">
+			  <div class="double-bounce1"></div>
+			  <div class="double-bounce2"></div>
+			</div>
+    	</div>
+				
+		<div class="nav-container">
+			<nav class="simple-bar top-bar">
+				<div class="container">
+				
+				
+					<div class="row nav-menu">
+						<div class="col-md-3 col-sm-3 columns">
+							<a href="index">
+							<img class="logo logo-dark" alt="Logo" src="img/logo-dark.png">
+						</div>
+					
+						<div class="col-md-9 col-sm-9 columns text-right">
+							<ul class="menu">
+							<a></a><li><a href="index" target="_self">Home</a></li>
+								<li><a href="about" target="_self">About us</a></li>
+								<li><a href="gallery" target="_self">Gallery</a></li>
+								<li><a href="contact" target="_self">Contact</a></li>
+								
+								
+								
+							</ul>
+		
+							
+						</div>
+					</div>
+					
+					<div class="mobile-toggle">
+						<i class="icon icon_menu"></i>
+					</div>
+					
+				</div>
+			</nav>
+		
+						
+			
+		
+			
+		
+			
+		
+			
+		
+			
+		
+			
+		
+						
+			
+		
+			
+		
+		</div>
+		
+		<div class="main-container">
+		<section class="team-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 text-center">
+							<h1>About Our Team</h1>
+						</div>
+					</div>
+					
+					<div class="row space-bottom-large">
+						<div class="col-sm-4">
+							<div class="team-1-member">
+								<img alt="Team Member" src="img/team_1.png ">
+								<h2>Sarah May</h2>
+								<h5>CEO</h5>
+								<p class="space-top-small">
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+								</p>
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="team-1-member">
+								<img alt="Team Member" src="img/team_2.png ">
+								<h2>Thomas Brown</h2>
+								<h5>Creative Director</h5>
+								<p class="space-top-small">
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+								</p>
+							</div>
+						</div>
+						
+						<div class="col-sm-4">
+							<div class="team-1-member">
+								<img alt="Team Member" src="img/team_3.png ">
+								<h2>Howard Branson</h2>
+								<h5>PR Specialist</h5>
+								<p class="space-top-small">
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+								</p>
+							</div>
+						</div>
+						
+		
+					</div>
+					
+					
+					
+				</div>
+			</section>
+			
+			<section class="primary-features duplicatable-content">
+				<div class="container">
+					<div class="row">
+		
+							<div class="col-md-4 col-sm-6 clearfix">
+								<div class="feature feature-icon-small">
+									<i class="icon icon-document"></i>
+									<h6 class="text-white"> Nemo enim ipsam voluptatem</h6>
+									<p class="text-white">
+										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor.
+									</p>
+								</div>
+							</div>
+						
+							<div class="col-md-4 col-sm-6 clearfix">
+								<div class="feature feature-icon-small">
+									<i class="icon icon-linegraph"></i>
+									<h6 class="text-white">Neque porro quisquam</h6>
+									<p class="text-white">
+										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor.
+									</p>
+								</div>
+							</div>
+						
+							<div class="col-md-4 col-sm-6 clearfix">
+								<div class="feature feature-icon-small">
+									<i class="icon icon-target"></i>
+									<h6 class="text-white">Excepteur occaecat cupidatat</h6>
+									<p class="text-white">
+										Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labor.
+									</p>
+								</div>
+							</div>
+		
+					</div>
+				
+				</div>
+			</section>
+		</div>
+		
+		<div class="footer-container">
+					
+			
+		
+			<footer class="details">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="logo" class="logo" src="img/logo-dark.png">
+							<p>
+								Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+							</p>
+						</div>
+						
+						<div class="col-sm-4">
+							<h1>Contact</h1>
+							<p>contact@realestate.com
+		<br>+70 424 495 952
+		<br>
+		<br>Kiewit Building
+		<br>200 Avenue Street
+		<br>Chicago</p>
+						</div>
+						
+						<div class="col-sm-4">
+							<h1>Social Profiles</h1>
+							<ul class="social-icons">
+								<li>
+									<a href="#">
+										<i class="icon social_twitter"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_facebook"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_instagram"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_dribbble"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_tumblr"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_pinterest"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-sm-12">
+							<span class="sub">© Copright 2014 Real Estate - All Rights Reserved. Free Website Template Designed by <a href=http://www.themezy.com>Themezy</a>.</span>
+						</div>
+					</div>
+					
+				</div>
+			</footer>
+		</div>
+				
+		<script src="https://www.youtube.com/iframe_api"></script>
+		<script src="js/jquery.min.js"></script>
+        <script src="js/jquery.plugin.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/skrollr.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/scrollReveal.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/twitterFetcher_v10_min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+				
---
+++ resources/views/index.blade.php
@@ -0,0 +1,405 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Real Estate Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="css/flexslider.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/line-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/elegant-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/theme-1.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+        <!--[if gte IE 9]>
+        	<link rel="stylesheet" type="text/css" href="css/ie9.css" />
+		<![endif]-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600,700%7CRaleway:700' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+    	<div class="loader">
+    		<div class="spinner">
+			  <div class="double-bounce1"></div>
+			  <div class="double-bounce2"></div>
+			</div>
+    	</div>
+				
+		<div class="nav-container">
+			<nav class="simple-bar top-bar">
+				<div class="container">
+				
+				
+					<div class="row nav-menu">
+						<div class="col-md-3 col-sm-3 columns">
+							<a href="index">
+							<img class="logo logo-dark" alt="Logo" src="img/logo-dark.png">
+						</div>
+					
+						<div class="col-md-9 col-sm-9 columns text-right">
+							<ul class="menu">
+								<a></a><li><a href="index" target="_self">Home</a></li>
+								<li><a href="about" target="_self">About us</a></li>
+								<li><a href="gallery" target="_self">Gallery</a></li>
+								<li><a href="contact" target="_self">Contact</a></li>
+								
+								
+								
+								
+							</ul>
+		
+							
+						</div>
+					</div>
+					
+					<div class="mobile-toggle">
+						<i class="icon icon_menu"></i>
+					</div>
+					
+				</div>
+			</nav>
+	
+		
+		</div>
+		
+		<div class="main-container">
+		<a id="hero-slider" class="in-page-link"></a>
+			
+			<section class="hero-slider">
+				<ul class="slides">
+					
+					
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/image_slider1.jpg">
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-md-6 col-sm-9">
+									<h1 class="text-white">Lorem ipsum dolor sit amet consectquis nostrud exercitation ullamco laboris nisi ut aliquip.</h1>
+									<a target="_blank" href="#" class="btn btn-primary btn-white">LEARN MORE</a>
+									
+								</div>
+							</div>
+						</div>
+					</li>
+					
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/image_slider2.jpg">
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-md-6 col-sm-9">
+									<h1 class="text-white">Voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae.</h1>
+									<a target="_blank" href="#" class="btn btn-primary btn-white">Learn more</a>
+									
+								</div>
+							</div>
+						</div>
+					</li>
+					
+					<li class="overlay">
+						<div class="background-image-holder parallax-background">
+							<img class="background-image" alt="Background Image" src="img/image_slider3.jpg">
+						</div>
+						
+						<div class="container align-vertical">
+							<div class="row">
+								<div class="col-md-6 col-sm-9">
+									<h1 class="text-white">Tempora incidunt ut labore dolore magnam aliquam voluptatem minima.</h1>
+									<a target="_blank" href="#" class="btn btn-primary btn-white">Learn more</a>
+									
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</section>
+			
+			<section class="feature-selector">
+				<div class="container">
+					<div class="row">
+						<ul class="selector-tabs clearfix">
+							<li class="clearfix text-primary col-md-3 col-sm-6 active">
+									<i class="icon icon-target"></i>
+									<span>Targeted Branding</span>
+							</li>
+						
+							<li class="clearfix text-primary col-md-3 col-sm-6">
+									<i class="icon icon-map-pin"></i>
+									<span>Geolocating Features</span>
+							</li>
+						
+							<li class="clearfix text-primary col-md-3 col-sm-6">
+									<i class="icon icon-strategy"></i>
+									<span>Marketing Strategy</span>
+							</li>
+						
+							<li class="clearfix text-primary col-md-3 col-sm-6">
+									<i class="icon icon-anchor"></i>
+									<span>On Page Navigation</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+				
+				<div class="container">
+					<ul class="selector-content">
+						<li class="clearfix active">
+							<div class="row">
+								<div class="col-sm-12 text-center">
+									<h1> During bond DOE last name von VFAUGHAN unless they put it on </h1>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-sm-6">
+									<p class="lead">
+									Alright, let me go ahead and give Royal Caribbean a call and verify their promotions. They have some really good promotions the last couple
+									</p>	
+								</div>
+							
+								<div class="col-sm-6">
+									<p class="lead">
+									full payment is due now the cruise line requires full payment you have price guarantee there's no certificate use on this one so it'd be 975 per cabin for two people
+									</p>	
+								</div>
+							</div>
+						</li>
+						
+						<li class="clearfix">
+							<div class="row">
+								<div class="col-sm-12 text-center">
+									<h1>Many appealing ways to present information</h1>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-sm-6">
+									<p class="lead">
+									Okay, so there are no balconies available. This is on the Royal Caribbean majesty of the seas. 
+									And we're doing a seven day trip to Costa Maya beliefs in Cozumel. The Ocean View rain one is going to come in least expensive as doing an ocean view guarantee. 
+									Going to come in at 359 per person. Having total with tax and everything on this one would come in at 975. 
+									We do guarantee that to be lowest price out there to me go ahead and grab that for you so
+									</p>	
+								</div>
+							
+								<div class="col-sm-6">
+									<p class="lead">
+									um, it came up as as just as a regular account, it didn't even have any additional promotion so you have the best price guarantee with this account.
+									</p>	
+								</div>
+							</div>
+						</li>
+						
+						<li class="clearfix">
+							<div class="row">
+								<div class="col-sm-12 text-center">
+									<h1>DO NOT spell out amounts. Right: $195 Wrong: a hundred and 95 dollars</h1>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-sm-6">
+									<p class="lead">
+									That sounds like a row. I have a prepaid. I have a prepaid to selling death certificate. You purchase. You don't even see it. No, this is what I get. So when I post The vacation villages thing this came with it.
+									</p>	
+								</div>
+							
+								<div class="col-sm-6">
+									<p class="lead">
+									Okay, let me see what that is. And it will get this take care of I'm so sorry this is happening just one moment. Okay, so you're with the village voyages and I 
+									</p>	
+								</div>
+							</div>
+						</li>
+						
+						<li class="clearfix">
+							<div class="row">
+								<div class="col-sm-12 text-center">
+									<h1>Numerous appealing ways to present information</h1>
+								</div>
+							</div>
+							
+							<div class="row">
+								<div class="col-sm-6">
+									<p class="lead">
+									If you have PHP installed locally and you would like to use PHP's built-in development server to serve your application, you may use the serve Artisan command. This command will start a development server at http://localhost:8000:
+
+php artisan serve
+									</p>	
+								</div>
+							
+								<div class="col-sm-6">
+									<p class="lead">
+										Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.
+									</p>	
+								</div>
+							</div>
+						</li>
+						
+		
+					</ul>
+				</div>
+			</section>
+			
+			<section class="no-pad-bottom no-pad-top projects-gallery">
+				
+				<div class="projects-wrapper clearfix">
+					
+					<div class="projects-container">
+						
+						<div class="col-md-4 col-sm-6 no-pad project print image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/interior_1.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h3 class="text-white"><strong>Kitchen</strong> Design</h3>
+									<p class="text-white">
+										Excepteur sint occaecat cupidatat non <br>proident sunt in culpa.
+									</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 no-pad project branding image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/interior_2.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h3 class="text-white"><strong>Living Room</strong> Design</h3>
+									<p class="text-white">
+										Excepteur sint occaecat cupidatat non <br>proident sunt in culpa.
+									</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+						
+						<div class="col-md-4 col-sm-6 no-pad project print image-holder">
+							<div class="background-image-holder">
+								<img class="background-image" alt="Background Image" src="img/interior_3.jpg">
+							</div>
+							<div class="hover-state">
+								<div class="align-vertical">
+									<h3 class="text-white"><strong>Bathroom</strong> Design</h3>
+									<p class="text-white">
+										Excepteur sint occaecat cupidatat non <br>proident sunt in culpa.
+									</p>
+									<a href="#" class="btn btn-primary btn-white">See Project</a>
+								</div>
+							</div>
+						</div>
+					
+					</div>
+					
+				</div>
+				
+			</section>
+		</div>
+		
+		<div class="footer-container">
+					
+			
+		
+			<footer class="details">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<img alt="logo" class="logo" src="img/logo-dark.png">
+							<p>
+								Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+							</p>
+						</div>
+						
+						<div class="col-sm-4">
+							<h1>Contact</h1>
+							<p>contact@realestate.com<br>+70 424 495 952<br>
+								<br>
+								Kiewit Building<br>
+								200 Avenue Street<br>
+								Chicago</p>
+						</div>
+						
+						<div class="col-sm-4">
+							<h1>Social Profiles</h1>
+							<ul class="social-icons">
+								<li>
+									<a href="#">
+										<i class="icon social_twitter"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_facebook"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_instagram"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_dribbble"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_tumblr"></i>
+									</a>
+								</li>
+								
+								<li>
+									<a href="#">
+										<i class="icon social_pinterest"></i>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-sm-12">
+							<span class="sub">© Copright 2014 Real Estate - All Rights Reserved. Free Website Template Designed by <a href=http://www.themezy.com>Themezy</a>.</span>
+						</div>
+					</div>
+					
+				</div>
+			</footer>
+		</div>
+				
+		<script src="https://www.youtube.com/iframe_api"></script>
+		<script src="js/jquery.min.js"></script>
+        <script src="js/jquery.plugin.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.flexslider-min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/skrollr.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/scrollReveal.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/twitterFetcher_v10_min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/jquery.countdown.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+				
